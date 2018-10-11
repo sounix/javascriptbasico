b1cbefa328b5cef97822bb572208e2876a4ad18f
sounix <sounix@gmail.com>
2018-10-11 10:36:16 -0500

    2018 1011 - Enseñando el Workflow

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
 var moment = require('moment');
 
-var a = 4;
-var b = 23;
-
-// console.log('SUMA :' + (a + b));
-
-console.log(`Fecha Actual: ${moment().format('MMMM Do YYYY, h:mm:ss a')}`);
+  console.log(`Fecha Actual: ${moment().format('MMMM Do YYYY, h:mm:ss a')}`);
